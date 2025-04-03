b2512c6ee2efb5a88bc685fe404719a64fb6f4a9
MikhailTsaritsyn <122229156+mikhailtsaritsyn@users.noreply.github.com>
2025-04-03 12:42:20 +0200

    Tweak: remove CPU features not yet used

--- include/CPU.hpp
+++ include/CPU.hpp
@@ -5,33 +5,11 @@
 #ifndef EMULATOR_MOS_6502_CPU_HPP
 #define EMULATOR_MOS_6502_CPU_HPP
 #include "Clock.hpp"
-#include "StatusRegister.hpp"
 #include <atomic>
-#include <cstdint>
 
 namespace emulator::mos_6502 {
 class CPU {
 public:
-    /**
-     * @brief The highest address available for the Stack
-     */
-    static constexpr uint16_t STACK_END = 0x01FF;
-
-    /**
-     * @brief Non-Maskable Interrupt vector
-     */
-    static constexpr uint16_t NMI = 0xFFFA;
-
-    /**
-     * @brief Reset vector
-     */
-    static constexpr uint16_t RES = 0xFFFC;
-
-    /**
-     * @brief Interrupt Request vector
-     */
-    static constexpr uint16_t IRQ = 0xFFFE;
-
     explicit CPU(std::chrono::nanoseconds clock_period) noexcept;
 
     /**
@@ -58,80 +36,6 @@ private:
      * @brief Block execution until the next high pulse arrives from @link _clock @endlink
      */
     void wait_for_clock() noexcept;
-
-    /**
-     * @brief Program counter
-     *
-     * The program counter keeps track of the memory location holding the current instruction code.
-     * Its content is automatically stepped up as the program is executed and is modified by branch and jump operations.
-     * As it must be able to address the full 16-bit address range of 64K bytes, it's the only 16-bit register of the
-     * 6502.
-     */
-    uint16_t PC = 0;
-
-    /**
-     * @brief Accumulator
-     *
-     * The accumulator is the main register of the 6502.
-     * Its content is typically used by the Arithmetic Logic Unit (ALU) for the first operand,
-     * and results are deposited in the accumulator again.
-     * Thus, its name, as results accumulate in this register.
-     * Most arithmetic and logical operations interact with this register.
-     */
-    uint8_t AC = 0;
-
-    /**
-     * @brief Index register X
-     *
-     * The X and Y registers are auxiliary registers.
-     * Like the accumulator, they can be loaded directly with values,
-     * both immediately, as literal constants, or from memory.
-     * Additionally, they can be incremented and decremented,
-     * and their contents may be transferred to and from the accumulator.
-     * Their main purpose is the use as index registers, where their contents are added to a base memory location,
-     * before any values are either stored to or retrieved from the resulting address,
-     * which is known as the effective address.
-     * This is commonly used for loops and table lookups at a given index, hence the name.
-     */
-    uint8_t X = 0;
-
-    /**
-     * @brief Index register Y
-     *
-     * The X and Y registers are auxiliary registers.
-     * Like the accumulator, they can be loaded directly with values,
-     * both immediately, as literal constants, or from memory.
-     * Additionally, they can be incremented and decremented,
-     * and their contents may be transferred to and from the accumulator.
-     * Their main purpose is the use as index registers, where their contents are added to a base memory location,
-     * before any values are either stored to or retrieved from the resulting address,
-     * which is known as the effective address.
-     * This is commonly used for loops and table lookups at a given index, hence the name.
-     */
-    uint8_t Y = 0;
-
-    /**
-     * @brief Stack pointer
-     *
-     * The stack pointer points to the current top of stack, or rather, to its bottom, as the stack grows top-down.
-     * The processor stack is located on memory page #1 ($0100–$01FF), 256 bytes Last-In-First-Out (LIFO) stack,
-     * which enables subroutines and also serves as a quick intermediate storage.
-     * As an 8-bit register, the stack pointer holds just the low-byte of this address (the offset from $0100.)
-     *
-     * @note It just wraps around in case that the stack underflows.
-     */
-    uint8_t SP = 0;
-
-    /**
-     * @brief Status register
-     *
-     * The status register holds the status of the processor, consisting of flags reflecting results of previous
-     * operations, configuration flags, like disabling interrupts or setting up Binary Coded Decimal mode (BCD),
-     * and the carry flag, which enables multibyte arithmetics.
-     *
-     * @note All arithmetic operations update the Z, N, C and V flags.
-     */
-    StatusRegister SR{};
 
     /**
      * @brief Pulse generator of the CPU.
