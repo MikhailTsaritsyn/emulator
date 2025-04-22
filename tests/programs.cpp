011749b5354d9c455f281145e1e1ed6831ef9440
MikhailTsaritsyn <122229156+mikhailtsaritsyn@users.noreply.github.com>
2025-04-22 22:31:11 +0200

    Test: refactor and add check for the resulting value

--- tests/programs.cpp
+++ tests/programs.cpp
@@ -16,48 +16,48 @@ struct Program : public ::testing::Test {
      * @brief An unsupported instruction to halt the CPU
      *
      * When the CPU encounters an illegal opcode, it terminates.
-     * It can be used in testing to terminate exactly after the provided code is executed.
+     * It can be used in testing to terminate right after the provided code is executed.
      */
     static constexpr uint8_t HLT = 0x02;
 
     /**
      * @brief Number of cycles elapsing during the startup routine
      */
-    static constexpr size_t STARTUP_LENGTH = 7;
+    static constexpr size_t STARTUP_DURATION = 7;
 
-    void SetUp() override {
-        _code        = {};
-        _code_length = 0;
+    /**
+     * @brief Prepare a chunk of memory containing a piece of code
+     *
+     * The code is prepended with @p CLI instruction to enable interrupts.
+     * The prepended code is inserted at the predefined @p PROGRAM_START address.
+     * The code is appended with an illegal @p HLT opcode to terminate the execution.
+     *
+     * @retval first Chunk of memory containing the code, prepared to run a CPU on it.
+     * @retval second Index in the resulting memory past the @p HLT opcode.
+     */
+    [[nodiscard]] static std::pair<Memory::Data, size_t> assemble(const std::vector<uint8_t> &code) noexcept {
+        Memory::Data result{};
+        if (code.size() > result.size()) panic("Code is too long to fit in memory");
+
+        result[PROGRAM_START] = 0x58; // CLI, to clear the interrupt disable flag set at startup
+        std::ranges::copy(code, result.begin() + PROGRAM_START + 1);
+
+        result[PROGRAM_START + 1 + code.size()] = HLT;
+
+        result[CPU::RES]     = low_byte(PROGRAM_START);
+        result[CPU::RES + 1] = high_byte(PROGRAM_START);
+        return { result, PROGRAM_START + code.size() + 2 };
     }
-
-    void TearDown() override {
-        Memory::Data data;
-        data[PROGRAM_START] = 0x58; // CLI, to clear the interrupt disable flag set at startup
-        std::ranges::copy(_code, data.begin() + PROGRAM_START + 1);
-
-        data[PROGRAM_START + 1 + _code.size()] = HLT;
-
-        data[CPU::RES]     = low_byte(PROGRAM_START);
-        data[CPU::RES + 1] = high_byte(PROGRAM_START);
-
-        CPU cpu(std::chrono::nanoseconds(0), Memory(data));
-        cpu.start();
-
-        EXPECT_EQ(cpu.program_counter(), PROGRAM_START + _code.size() + 2); // 1 for CLI and 1 for HLT
-        EXPECT_EQ(cpu.cycle(), _code_length + STARTUP_LENGTH + 3);          // 2 for CLI and 1 for HLT
-    }
-
-    std::vector<uint8_t> _code;
-    size_t _code_length = 0; ///< Number of cycles elapsed to execute the code
 };
 
 TEST_F(Program, Empty) {}
 
 // https://web.archive.org/web/20221112220344if_/http://archive.6502.org/datasheets/synertek_programming_manual.pdf
-// Example 2.3
+// Example 2.3:
 // first number at {H1, L1}
 // second number at {H2, L2}
 // result written to {H3, L3}
+//
 // LDA L1
 // CLC
 // ADC L2
@@ -73,38 +73,60 @@ TEST_F(Program, Add16Bit) {
     constexpr uint8_t L3 = 0x04;
     constexpr uint8_t H3 = 0x05;
 
+    std::vector<uint8_t> code;
+    size_t code_duration = 0;
+
     // LDA L1
-    _code.push_back(0xA5); // LDA zero page: 2 bytes, 3 cycles
-    _code.push_back(L1);
-    _code_length += 3;
+    code.push_back(0xA5); // LDA zero page: 2 bytes, 3 cycles
+    code.push_back(L1);
+    code_duration += 3;
 
     // CLC
-    _code.push_back(0x18); // CLC: 1 byte, 2 cycles
-    _code_length += 2;
+    code.push_back(0x18); // CLC: 1 byte, 2 cycles
+    code_duration += 2;
 
     // ADC L2
-    _code.push_back(0x65); // ADC zero page: 2 bytes, 3 cycles
-    _code.push_back(L2);
-    _code_length += 3;
+    code.push_back(0x65); // ADC zero page: 2 bytes, 3 cycles
+    code.push_back(L2);
+    code_duration += 3;
 
     // STA L3
-    _code.push_back(0x85); // STA zero page: 2 bytes, 3 cycles
-    _code.push_back(L3);
-    _code_length += 3;
+    code.push_back(0x85); // STA zero page: 2 bytes, 3 cycles
+    code.push_back(L3);
+    code_duration += 3;
 
     // LDA H1
-    _code.push_back(0xA5); // LDA zero page: 2 bytes, 3 cycles
-    _code.push_back(H1);
-    _code_length += 3;
+    code.push_back(0xA5); // LDA zero page: 2 bytes, 3 cycles
+    code.push_back(H1);
+    code_duration += 3;
 
     // ADC H2
-    _code.push_back(0x65); // ADC zero page: 2 bytes, 3 cycles
-    _code.push_back(H2);
-    _code_length += 3;
+    code.push_back(0x65); // ADC zero page: 2 bytes, 3 cycles
+    code.push_back(H2);
+    code_duration += 3;
 
     // STA H3
-    _code.push_back(0x85); // STA zero page: 2 bytes, 3 cycles
-    _code.push_back(H3);
-    _code_length += 3;
+    code.push_back(0x85); // STA zero page: 2 bytes, 3 cycles
+    code.push_back(H3);
+    code_duration += 3;
+
+    // insert the code to the memory
+    auto [data, program_end] = assemble(code);
+
+    // initialize the arguments
+    data[L1] = 0x93;
+    data[L2] = 0x75;
+    data[H1] = 0x03;
+    data[H2] = 0x34;
+
+    // execute the program
+    CPU cpu(std::chrono::nanoseconds(0), Memory(data));
+    cpu.start();
+
+    // check the results
+    EXPECT_EQ(cpu.program_counter(), program_end);                // 1 for CLI and 1 for HLT
+    EXPECT_EQ(cpu.cycle(), code_duration + STARTUP_DURATION + 3); // 2 for CLI and 1 for HLT
+    EXPECT_EQ(cpu.memory()[L3], 0x08);                            // (data[L1] + data[L2]) % 0x100
+    EXPECT_EQ(cpu.memory()[H3], 0x38);                            // data[H1] + data[H2] + carry
 }
 } // namespace emulator::mos_6502::test
